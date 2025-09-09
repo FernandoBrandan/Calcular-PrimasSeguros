3239d799c5645fd968eafdec9c645664b8f3503d
fernandobrandan <fernando.brandan1@hotmail.com>
2025-09-09 16:31:55 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,14 @@
 import { ejemploCalculoPrima as b } from "./use-cases/execBase"
 import { ejemploCalculoPrima as p } from "./use-cases/execPesonalized"
 
-// Ejecutar el ejemplo base o personalizado según se desee
-// Aquí se ejecuta el base, pero puedes cambiar a 'p()' para el personalizado
 
 if (require.main === module) {
 
-    b()
-        .then(resultado => console.log('Cálculo completado:', resultado))
-        .catch(error => {
-            console.error('Error en el cálculo:', error)
-            process.exit(1)
-        }).finally(() => {
-            console.log("=== Fin del Cálculo de Prima Base ===\n\n")
-        })
-
-
     p()
+        // b()
         .then(resultado => console.log('Cálculo completado:', resultado))
         .catch(error => {
             console.error('Error en el cálculo:', error)
             process.exit(1)
-        }).finally(() => {
-            console.log("=== Fin del Cálculo de Prima Personalizada ===")
         })
-
-} 
+}
